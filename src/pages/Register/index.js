411b1db997b453497e8958c1d870fbebfcae85c3
Diego Sosa <dhsosa98@gmail.com>
2021-12-25 17:27:19 -0300

    added name, surname, and a success window modal

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,21 +1,54 @@
-import FormUser from '../../components/FormUser'
 import { useState } from 'react'
 import { useHistory } from "react-router"
 import { registerUser } from '../../services/register'
+import FormUserRegister from '../../components/FormUserRegister'
+import ModalWindow from '../../components/ModalWindow'
 
 const Register = () => {
-const [username, setUsername] = useState('')
-const [password, setPassword] = useState('')
+
+const [user, setUser] = useState({
+    name: "",
+    surname: "",
+    username: "",
+    password: ""
+})
 const history = useHistory()
 
-const handleRegisterUser = (e) => {
+const modalText = "User registered successfully"
+
+const [open, setOpen] = useState(false)
+
+const [error, setError] = useState('')
+
+const handleRegisterUser = async (e) => {
+    try{
     e.preventDefault()
-    const user = { username, password }
-    registerUser(history, user)
+    const {data} = await registerUser(user)
+    if (data){
+        setOpen(true) 
+    }
+    }
+    catch(error){
+        setError("This username has been used")
+    }
+    
 }
 
+const handleChangeUser = (e) => {
+    setUser({...user, [e.target.name]: e.target.value})
+    setError('')
+    console.log(user)
+}
+
+const handleConfirm = () => {
+    setOpen(false)
+    history.push("/login")
+}
     return (
-        <FormUser onSubmit={handleRegisterUser} setUsername={setUsername} setPassword={setPassword} />
+        <>
+        <FormUserRegister onSubmit={handleRegisterUser} handleUser={handleChangeUser} error={error}/>
+        <ModalWindow modalText={modalText} open={open} setOpen={open} handleConfirm={handleConfirm} type="confirm"/>
+        </>
     )
 }
 
