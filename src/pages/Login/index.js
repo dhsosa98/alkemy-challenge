b7c68815fcefa35fa3d0f1add168e0289f8ce46b
Diego Sosa <dhsosa98@gmail.com>
2021-12-25 17:28:44 -0300

    styled Login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,26 +1,38 @@
-import { useState } from 'react'
-import FormUser from '../../components/FormUser'
+import { useState, useContext} from 'react'
+import FormUserLogin from '../../components/FormUserLogin'
 import { getUser } from '../../services/login'
-import { useHistory } from 'react-router-dom'
+import { useHistory} from 'react-router-dom'
+import { AuthContext } from '../../AuthContext'
 
 
 const Login = () => {
 const [username, setUsername] = useState('')
 const [password, setPassword] = useState('')
+const [error, setError] = useState(false)
 const history = useHistory()
+const {signin} = useContext(AuthContext)
 
-    const handleLoginUser = (e) => {
-        e.preventDefault()
+    const handleLoginUser = async (e) => {
+        try{
+        e.preventDefault() 
         const user = {
             username: username,
             password: password
-          }
-        getUser(user, history)
+        }
+        const {data} = await getUser(user)
+        if (data.token){    
+            signin(data)
+            history.push("/")
+        }
+     }
+        catch{
+            setError(true)
+        }  
     }
 
    
     return (
-        <FormUser onSubmit={handleLoginUser} setUsername={setUsername} setPassword={setPassword} />
+        <FormUserLogin onSubmit={handleLoginUser} setUsername={setUsername} setPassword={setPassword} error={error} setError={setError} />
     )
 }
 
