a7cc3b0960a6cfdffb9abde3b273797acd90e0ce
Diego Sosa <dhsosa98@gmail.com>
2022-01-06 12:26:55 -0300

    updated tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+   darkMode: false, // or 'media' or 'class'
+   theme: {
+     extend: {},
+   },
+   variants: {
+     extend: {},
+   },
+   plugins: [],
+ }
