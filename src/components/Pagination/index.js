85567c3d33df435047b312c72907b4028cc0bf9d
Diego Sosa <dhsosa98@gmail.com>
2022-01-06 12:56:51 -0300

    changed the logical of pagination with querys

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,70 +1,89 @@
 import { useEffect } from 'react'
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
+import { useHistory } from 'react-router-dom'
+import { ChevronLeftIcon, ChevronRightIcon} from '@heroicons/react/solid'
 
-const Pagination = ({pageLimit, setPageLimit, cantOperations}) => {
-    const cantLimit = 10
-    const handleChangePagePrev = () =>{
-        if (pageLimit.prev>0){
-            setPageLimit({prev: pageLimit.prev-cantLimit, next: pageLimit.prev-1})
-        }
-    }
-    const handleChangePageNext = () =>{
-        if (pageLimit.next<=cantOperations-2){
-        setPageLimit({prev: pageLimit.next+1, next: pageLimit.next+cantLimit})
-        }
-    }
+const Pagination = ({...props}) => {
+  const {page, size, cantOperations, querys} = props
+  console.log(Math.ceil(cantOperations / size)-1)
+  const history = useHistory()
+  let pageLimit = { prev: ((page+1)*size-size)+1, next: (page+1)*size }
+    
     useEffect(() => {
-        if (cantOperations % cantLimit === 0){
-            if (pageLimit.prev > 0){
-                setPageLimit({prev: pageLimit.prev-cantLimit, next: pageLimit.prev-1})
+        if (cantOperations % size === 0){
+            if (pageLimit.prev > 10){
+                querys.set("page", page-1)
+                history.push(`/?${querys.toString()}`)
             }
         }
-    }, [cantOperations, pageLimit, setPageLimit])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [cantOperations]) 
+
+    useEffect(() => {
+      querys.get("page") < 0 ? querys.set("page", 0) :
+      (querys.get("page")  > (Math.ceil(cantOperations / size)-1)) && cantOperations 
+      && querys.set("page", Math.ceil(cantOperations / size))
+      history.push(`/?${querys.toString()}`)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [querys]) 
+
+    const handleNavigate = (e, pageLink) => {
+      e.preventDefault()
+      querys.set("page", pageLink)
+      history.push(`/?${querys.toString()}`)
+    }
     return (
         <>
-        {cantOperations>0 && 
         <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
-        
-        {cantOperations>cantLimit && 
+        {cantOperations>size &&
         <div className=" container relative sm:hidden">
-            {pageLimit.prev >= cantLimit &&
+            {pageLimit.prev > size &&
+          <div>
           <button
-            onClick={handleChangePagePrev}
+          onClick={(e)=>handleNavigate(e, page-1)}
             className="absolute items-center left-0 px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50"
           >
             Previous
           </button>
+          </div>
             }
-          {pageLimit.next+1<cantOperations &&
+          {pageLimit.next<cantOperations &&
           <button
-            onClick={handleChangePageNext}
+          onClick={(e)=>handleNavigate(e, page+1)}
             className="ml-3 absolute right-0 items-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50"
           >
             Next
-          </button>}
+          </button>
+          }
           </div>
-        }      
+        }  
+        {(cantOperations<=0) &&
+        <div className='mx-auto'>
+          <p className="text-sm text-gray-700">
+          Nothing to Show
+          </p>
+        </div>}    
         <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-          <div>
-            <p className="text-sm text-gray-700">
-              Showing <span className="font-medium">{pageLimit.prev+1}</span> to <span className="font-medium">{pageLimit.next<cantOperations ? `${pageLimit.next+1}` :`${cantOperations}`}</span> of{' '}
+          {cantOperations > 0 &&
+            (<p className="text-sm text-gray-700">
+              Showing <span className="font-medium">{pageLimit.prev}</span> to <span className="font-medium">{pageLimit.next<cantOperations ? `${pageLimit.next}` :`${cantOperations}`}</span> of{' '}
               <span className="font-medium">{cantOperations}</span> operations
-            </p>
-          </div>
+            </p>)
+           
+          }
           <div>
-          {cantOperations>cantLimit &&
+          {cantOperations>size &&
             <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
-                {pageLimit.prev >= cantLimit &&
+                {pageLimit.prev > size &&
               <button
-                onClick={handleChangePagePrev}
+                onClick={(e)=>handleNavigate(e, page-1)}
                 className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-indigo-600 bg-white text-sm font-medium text-indigo-600 hover:bg-gray-50"
               >
                 <span className="sr-only">Previous</span>
                 <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
               </button>}
-              {pageLimit.next+1<cantOperations &&
+              {pageLimit.next<cantOperations &&
               <button
-                onClick={handleChangePageNext}
+                onClick={(e)=>handleNavigate(e, page+1)}
                 className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-indigo-600 bg-white text-sm font-medium text-indigo-600 hover:bg-gray-50"
               >
                 <span className="sr-only">Next</span>
@@ -74,7 +93,7 @@ const Pagination = ({pageLimit, setPageLimit, cantOperations}) => {
             }
           </div>
         </div>
-      </div>}
+      </div>
       </>
     )
 }
