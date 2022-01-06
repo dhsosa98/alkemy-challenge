d9b82f2c3c8b6ea694db7d58f5a0462aed819ab5
Diego Sosa <dhsosa98@gmail.com>
2022-01-06 12:46:32 -0300

    added showPass state and refactored component

--- src/components/FormUserLogin/index.js
+++ src/components/FormUserLogin/index.js
@@ -1,7 +1,11 @@
+import { EyeIcon, EyeOffIcon, LoginIcon } from '@heroicons/react/outline'
 import { CashIcon } from '@heroicons/react/solid'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
-const FormUserLogin = ( { onSubmit, setUsername, setPassword, error, setError } ) => {
+const FormUserLogin = ( {...props} ) => {
+  const { onSubmit, setUsername, setPassword, error, setError } = props
+  const [showPass, setShowPass] = useState(false)
     return (
         
         <>
@@ -12,7 +16,7 @@ const FormUserLogin = ( { onSubmit, setUsername, setPassword, error, setError } 
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Log in to your account</h2>
           </div>
           <form className="mt-8 space-y-6" onSubmit={onSubmit}>
-            {error && <div className=' bg-red-500 text-center p-2 text-white'>Username and/or password incorrect</div>}
+            {error && <div className=' bg-red-500 text-center p-2 text-white'>{error}</div>}
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
@@ -23,7 +27,7 @@ const FormUserLogin = ( { onSubmit, setUsername, setPassword, error, setError } 
                   name="username"
                   autoComplete="current-username"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none sm:text-sm"
                   placeholder="Username"
                   onChange={(e)=>{
                     setError(false)
@@ -31,21 +35,28 @@ const FormUserLogin = ( { onSubmit, setUsername, setPassword, error, setError } 
                   }}
                 />
               </div>
-              <div>
+              <div className='flex flex-row border border-gray-300 rounded-b-md'>
                 <label className="sr-only">
                   Password
                 </label>
-                <input
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Password"
-                  onChange={(e)=>{
-                    setError(false)
-                    setPassword(e.target.value)}}
-                />
+                  <input
+                    name="password"
+                    type={!showPass ? "password" : "text"}
+                    autoComplete="current-password"
+                    required
+                    className="appearance-none border-none relative block w-full px-3 py-2 rounded-b-md  placeholder-gray-500 text-gray-900 focus:outline-none sm:text-sm"
+                    placeholder="Password"
+                    onChange={(e)=>{
+                      setError(false)
+                      setPassword(e.target.value)}}
+                  />
+                  <div className='pt-1.5 px-3'>
+                    {!showPass ?
+                    <EyeIcon onClick={()=>setShowPass(!showPass)} className='h-6 text-gray-500' />
+                    :
+                    <EyeOffIcon onClick={()=>setShowPass(!showPass)} className='h-6 text-gray-500' />
+                    }
+                </div>
               </div>
             </div>
 
@@ -63,9 +74,9 @@ const FormUserLogin = ( { onSubmit, setUsername, setPassword, error, setError } 
               </div>
 
               <div className="text-sm">
-                <a href="/" className="font-medium text-indigo-600 hover:text-indigo-500">
+                <Link to="/reset-password" className="font-medium text-indigo-600 hover:text-indigo-500">
                   Forgot your password?
-                </a>
+                </Link>
               </div>
             </div>
 
@@ -74,6 +85,9 @@ const FormUserLogin = ( { onSubmit, setUsername, setPassword, error, setError } 
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <LoginIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                </span>
                 Log in
               </button>
               <div className="text-lg text-center m-2">
