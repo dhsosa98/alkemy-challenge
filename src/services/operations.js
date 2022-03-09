cd92967b02681d04ed39d242dce77fe98aca9d33
Diego Sosa <dhsosa98@gmail.com>
2022-03-08 22:21:54 -0300

    changed to localStorage

--- src/services/operations.js
+++ src/services/operations.js
@@ -8,7 +8,7 @@ let APIURL
 const baseURL = `${APIURL}/api/operations`
 
 const submitOperationItem = async (isEdit, newOperationItem) => {
-const config = {headers: {authorization: sessionStorage.getItem('token')}}
+const config = {headers: {authorization: localStorage.getItem('token')}}
 if (!isEdit){
         return axios.post(baseURL, newOperationItem, config).then(res => res.data) 
     }
@@ -18,13 +18,13 @@ if (!isEdit){
 }
 
 const deleteOperationItem = async (id) => {
-  const config = {headers: {authorization: sessionStorage.getItem('token')}}
+  const config = {headers: {authorization: localStorage.getItem('token')}}
   return axios.delete(baseURL+'/'+id, config).then(res => res.data)
 }
 
 const getOperationList = async (query) => {
   try{
-    const config = {headers: {authorization: sessionStorage.getItem('token')}}
+    const config = {headers: {authorization: localStorage.getItem('token')}}
     const {data} = await axios.get(baseURL+'/userOperations?'+query, config)
     return data.data.operations}
   catch{}
