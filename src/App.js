1f2e0db4ab30b6fa5b908e59ddaf8f686bd4494e
Diego Sosa <dhsosa98@gmail.com>
2022-03-08 22:19:55 -0300

    added Queries Context to app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import { BrowserRouter as Router, Switch} from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import ResetPass from './pages/Reset-pass'
-import PrivateRoute from './routes/PrivateRoute'
-import PublicRoute from './routes/PublicRoute'
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ResetPass from "./pages/Reset-pass";
+import { QueriesProvider } from "./QueriesContext";
+import PrivateRoute from "./routes/PrivateRoute";
+import PublicRoute from "./routes/PublicRoute";
 
-export default function App()  {
-    return (
-        <Router>
-            <Switch>
-                <PublicRoute exact path="/login" component={Login} />
-                <PublicRoute exact path="/register" component={Register} />
-                <PublicRoute exact path="/reset-password"  component={ResetPass} />
-                <PrivateRoute path="/" component={Home} />
-            </Switch>     
-        </Router>
-    )   
+export default function App() {
+  return (
+    <Router>
+      <QueriesProvider>
+        <Switch>
+          <PublicRoute exact path="/login" component={Login} />
+          <PublicRoute exact path="/register" component={Register} />
+          <PublicRoute exact path="/reset-password" component={ResetPass} />
+          <PrivateRoute path="/" component={Home} />
+        </Switch>
+      </QueriesProvider>
+    </Router>
+  );
 }
-
